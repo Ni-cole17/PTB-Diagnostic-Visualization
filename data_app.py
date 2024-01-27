b737f908b7a480c0c25a4aa73acf6e05948fd172
Ni.cole17 <nicole.charron17@gmail.com>
2024-01-27 19:20:43 -0300

    Update requirements and app

--- data_app.py
+++ data_app.py
@@ -35,6 +35,11 @@ def filt_bandpass(sig,fs):
     return sig_filt
 
 def make_matrix(signal):
+    '''
+    Função que recebe um sinal e retorna a matriz de distâncias.
+    :param signal: sinal a ser transformado em matriz de distâncias.
+    :return l: matriz de distâncias.
+    '''
     if switch2.active == 0:
         signal = signal[:,:12]
     elif switch2.active == 1:
@@ -56,13 +61,24 @@ def make_matrix(signal):
     return l
 
 def read_signal(patient, hea, n_channels = 15):
+    '''
+    Função que recebe o nome do paciente e o nome do exame e retorna o sinal e os campos do exame.	
+    :param patient: nome do paciente.
+    :param hea: nome do exame.
+    :param n_channels: número de canais do exame.
+    :return signal: sinal do exame.
+    '''
     signal, fields = wfdb.rdsamp(f'data/ptb-diagnostic-ecg-database-1.0.0/{patient}/{hea}', channels=[*range(n_channels)])
     return signal, fields
 
 
 def generate_hist(event):
+    '''
+    Função que recebe um evento e atualiza o histograma.
+    :param event: evento que ocorreu.
+    '''
     global quantile_vbars
-    global matrix1
+    global matrix
     x_range, y_range = p4.x_range, p4.y_range
     xmin, xmax = x_range.start, x_range.end
     ymax, ymin = y_range.start, y_range.end
@@ -73,7 +89,7 @@ def generate_hist(event):
     ymax = min(2000, int(ymax))
     
     # Update the histogram data based on the current image view
-    rounded_signal = np.round(matrix1,3)
+    rounded_signal = np.round(matrix,3)
     rounded_signal = rounded_signal[int(ymin):int(ymax), int(xmin):int(xmax)]
 
     ## Update Metrics
@@ -98,26 +114,35 @@ def generate_hist(event):
 
 
 def update_spinner_end(attrname, old, new):
+    '''
+    Função que recebe um evento e atualiza o spinner de fim.
+    '''
+    global spinner_end
     if spinner_start.value + 2000 <= spinner_start.high:
-        spinner_end.value = spinner_start.value + 2000
-        spinner_end.high = spinner_start.value + 2000
+        spinner_end = spinner_start.value + 2000
     else: 
-        spinner_end.value = spinner_start.high
+        spinner_end = spinner_start.high
 
 
 def callback(event):
-    global matrix1
+    '''
+    Função que recebe um evento e atualiza o gráfico de distâncias.
+    '''
+    global matrix
     if event.x and event.y:
         x_value = event.y 
         y_value = event.x  # Invert y due to plot coordinate system
 
         p11_line = p11.renderers[0] 
-        p11_line.data_source.data = dict(x=matrix1[:, int(y_value)],y=range(0,2000))
+        p11_line.data_source.data = dict(x=matrix[:, int(y_value)],y=list(range(0,2000)))
 
         p1_line = p1.renderers[0] 
-        p1_line.data_source.data = dict(x=range(0,2000),y=matrix1[int(x_value),:])
+        p1_line.data_source.data = dict(x=list(range(0,2000)),y=matrix[int(x_value),:])
 
 def update_patient(attrname, old, new):
+    '''
+    Função que recebe um evento e atualiza o dropdown de pacientes.
+    '''
     selected_patology = dropdown_patology.value
     if selected_patology != "All":  
         dropdown_pat.options = ["None"]+ list(df[df['Reason for admission'] == selected_patology]["idP"])
@@ -125,15 +150,23 @@ def update_patient(attrname, old, new):
         dropdown_pat.options = ["None"]+ list(df["idP"])
 
 def update_heas(attrname, old, new):
+    '''
+    Função que recebe um evento e atualiza o dropdown de exames.
+    '''
     selected_patient = dropdown_pat.value
     dropdown_exam.options = ["None"]+data[selected_patient]["heas"]
 
 def update_signal(attrname, old, new):
+    '''
+    Função que recebe um evento e atualiza o sinal.
+    '''
     global signals_t
     global len_signal
+    global spinner_end
+
     selected_signal = dropdown_exam.value
     filtered = switch3.active
-    if spinner_start.value == spinner_end.value:
+    if spinner_start.value == spinner_end:
         signals_t = np.zeros((2000,15))
     elif selected_signal != "None":
         signals_t,_ = read_signal(dropdown_pat.value,selected_signal.replace(".hea",""))
@@ -141,27 +174,27 @@ def update_signal(attrname, old, new):
             signals_t = filt_bandpass(signals_t,1000)
     else:
         signals_t = np.zeros((2000,15))
-    len_signal = signals_t.shape[0]
-    spinner_start.value = 0
-    spinner_start.high = len_signal
 
 def generate_matrix(attrname, old, new):
+    '''
+    Função que recebe um evento e atualiza a matriz de distâncias.
+    '''
     global signals_t
     global matrix
-    global matrix1
+    global spinner_end
 
-    signals = signals_t[spinner_start.value:spinner_end.value]
+    signals = signals_t[spinner_start.value:spinner_end]
     signal_channel = signals[:, spinner_channel.value]
     source.data = dict(x=np.arange(len(signal_channel)), y=signal_channel)
-    matrix1 = make_matrix(signals)
-    matrix = np.flipud(matrix1)
-    hover_tool.callback = CustomJS(code=hover_code, args={'matrix1': matrix1})
-    if spinner_end.value - spinner_start.value == 2000 or spinner_end.value - spinner_start.value == 0: 
-        p4.title.text = f"{dropdown_pat.value} | {dropdown_patology.value} | {dropdown_exam.value} | Channel: {spinner_channel.value} | Start: {spinner_start.value} | End: {spinner_end.value}"
-        if spinner_end.value - spinner_start.value == 0:
-            p4.image(image=[np.zeros((2000,2000))], x=0, y=2000, dw=2000, dh=2000, palette="Turbo256")
+    matrix = make_matrix(signals)
+    #matrix = np.flipud(matrix1)
+    hover_tool.callback = CustomJS(code=hover_code, args={'matrix': matrix})
+    if spinner_end - spinner_start.value == 2000 or spinner_end - spinner_start.value == 0: 
+        p4.title.text = f"{dropdown_pat.value} | {dropdown_patology.value} | {dropdown_exam.value} | Channel: {spinner_channel.value} | Start: {spinner_start.value} | End: {spinner_end}"
+        if spinner_end - spinner_start.value == 0:
+            p4.image(image=[np.zeros((2000,2000))], x=0, y=0, dw=2000, dh=2000, palette="Turbo256")
         else:
-            p4.image(image=[matrix], x=0, y=2000, dw=2000, dh=2000, palette="Turbo256")
+            p4.image(image=[matrix], x=0, y=0, dw=2000, dh=2000, palette="Turbo256")
 
         p2_line = p2.renderers[0] 
         p2_line.data_source.data = dict(x=np.arange(len(signal_channel)), y=signal_channel)
@@ -173,23 +206,26 @@ def generate_matrix(attrname, old, new):
         generate_hist(event=None)
 
 def generate_matrix_thresh(attrname, old, new):
+    '''
+    Função que recebe um evento e atualiza a matriz de distâncias com limiar.
+    '''
     global matrix
-    global matrix1
+    global spinner_end
     if switch.active == 1:
         mask = (matrix < thresh_value_up.value) & (matrix > thresh_value_down.value)
         mask = (~mask*255).astype(np.uint8)
 
-        p4.title.text = f"{dropdown_pat.value} | {dropdown_patology.value} | {dropdown_exam.value} | Channel: {spinner_channel.value} | Start: {spinner_start.value} | End: {spinner_end.value}"
-        p4.image(image=[mask], x=0, y=2000, dw=2000, dh=2000, palette="Greys256")
+        p4.title.text = f"{dropdown_pat.value} | {dropdown_patology.value} | {dropdown_exam.value} | Channel: {spinner_channel.value} | Start: {spinner_start.value} | End: {spinner_end}"
+        p4.image(image=[mask], x=0, y=0, dw=2000, dh=2000, palette="Greys256")
     else:
-        p4.image(image=[matrix], x=0, y=2000, dw=2000, dh=2000, palette="Turbo256")
+        p4.image(image=[matrix], x=0, y=0, dw=2000, dh=2000, palette="Turbo256")
 
 
 ## MAIN
 
 global signals_t
-global matrix1
 global quantile_vbars
+global spinner_end
 
 ## Reading info data
 df = pd.read_csv('data/Record_info.csv')
@@ -211,19 +247,20 @@ fs = 1000
 signals_t,_ = read_signal(list(data.keys())[0],data[list(data.keys())[0]]["heas"][0].replace(".hea",""))
 signals = signals_t[0:2000]
 signal_channel = signals[:, 0]
-matrix1 = make_matrix(signals)
-matrix = np.flipud(matrix1) 
+matrix = make_matrix(signals)
+#matrix1 = make_matrix(signals)
+#matrix = np.flipud(matrix1) 
 rounded_signal = np.round(matrix,3)
 hist_values, hist_bins = np.histogram(rounded_signal,bins=6200)
-max_value = np.max(matrix1)
-min_value = np.min(matrix1)
-quantiles = np.quantile(matrix1, [0.1, 0.25, 0.5, 0.75, 0.9])
+max_value = np.max(matrix)
+min_value = np.min(matrix)
+quantiles = np.quantile(matrix, [0.1, 0.25, 0.5, 0.75, 0.9])
 
 # Save matrix in a raw file
-file_path = "matrix_data.pkl"
+'''file_path = "matrix_data.pkl"
 output = open(file_path, 'wb')
-pickle.dump(matrix1, output)
-output.close()
+pickle.dump(matrix, output)
+output.close()'''
 
 # Creating Widgets for app
 
@@ -234,7 +271,7 @@ hover_code = """
     const y = geometry.y;
 """
 
-hover_tool = HoverTool(tooltips=[("Value", "@image"),("(x,y)", "($x, $y)")],callback=CustomJS(args={'matrix1': matrix1}, code=hover_code))
+hover_tool = HoverTool(tooltips=[("Value", "@image"),("(x,y)", "($x, $y)")],callback=CustomJS(args={'matrix': matrix}, code=hover_code))
 
 ## Dropdown patology
 valid_options_patology = pd.notna(df['Reason for admission'])
@@ -250,7 +287,7 @@ dropdown_exam = Select(title="Selecione o Exame", value=data[selected_patient]["
 
 ## Spinner
 spinner_start = Spinner(title="Selecione o ponto de inicio do sinal:", low=0, high=signals_t.shape[0], step=1, value=0)
-spinner_end = Spinner(title="Signal o ponto de término do sinal:", low=0, high=signals_t.shape[0], step=1, value=2000)
+spinner_end = spinner_start.value + 2000# Spinner(title="S o ponto de término do sinal:", low=0, high=signals_t.shape[0], step=1, value=2000)
 spinner_channel = Spinner(title="Selecione um Canal :", low=0, high=signals_t.shape[1]-1, step=1, value=0)
 thresh_value_down = Spinner(title="Escolha um limiar inferior para o gráfico de recorrência",low=-1, high=20, step=0.01, value = -1)
 thresh_value_up = Spinner(title="Escolha um limiar superior para o gráfico de recorrência",low=0, high=20, step=0.01, value = 0.3)
@@ -267,7 +304,7 @@ dropdown_exam.on_change('value', generate_matrix)
 spinner_start.on_change('value', update_spinner_end)
 spinner_start.on_change('value', generate_matrix)
 
-spinner_end.on_change('value', generate_matrix)
+#spinner_end.on_change('value', generate_matrix)
 spinner_channel.on_change('value', generate_matrix)
 
 thresh_value_up.on_change('value', generate_matrix_thresh)
@@ -299,11 +336,11 @@ p = PreText(text=f"Valor max: {max_value:.2f}\nValor min: {min_value:.2f}\n\
 75%:{quantiles[3]:.2f}\n90%: {quantiles[4]:.2f}",width=200, height=100)
 
 ## Defining figure type for each plot
-p1.line(range(0,2000), np.zeros(2000), line_width=2)
-p11.line(np.zeros(2000),range(0,2000), line_width=2)
+p1.line(list(range(0,2000)), np.zeros(2000), line_width=2)
+p11.line(np.zeros(2000),list(range(0,2000)), line_width=2)
 p2.line(np.arange(len(signal_channel)), signal_channel)
 p3.line(signal_channel, np.arange(len(signal_channel)))
-p4.image(image=[matrix], x=0, y=2000, dw=2000, dh=2000,palette="Turbo256")
+p4.image(image=[matrix], x=0, y=0, dw=2000, dh=2000,palette="Turbo256")
 p5.quad(top=hist_values, bottom=0, left=hist_bins[:-1], right=hist_bins[1:], fill_color="navy", line_color="navy")
 quantile_vbars = [p5.vbar(x=quantiles[i], top=max(hist_values), width=0.01, color="red", legend_label=f"Quantile {q*100}%: {quantiles[i]:.2f}") for i, q in enumerate([0.1, 0.25, 0.5, 0.75, 0.9])]
 
@@ -333,8 +370,7 @@ p4.add_tools(hover_tool)
 grid = gridplot([[p,p2,p5], [p3, p4, p11],[None,p1]])
 
 ## Adding to document
-curdoc().add_root(column(dropdown_patology,dropdown_pat,dropdown_exam,spinner_channel,spinner_start,spinner_end,switch,switch2,switch3,thresh_value_up,thresh_value_down,grid))
-
+curdoc().add_root(column(dropdown_patology,dropdown_pat,dropdown_exam,spinner_channel,spinner_start,switch,switch2,switch3,thresh_value_up,thresh_value_down,grid))
 
 ## Activate python venv before running the app with powershell: .\venv\Scripts\Activate.ps1
 ##To run the app use the command: bokeh serve --show data_app.py
